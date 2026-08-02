f27d243945c12b80ae3aa295939123dfefe119c4
Sandes <sandes.savarimuthu@epitech.eu>
2026-08-03 00:29:44 +0200

    feat: wire up routing for memories and map

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,10 @@ import { HomePage } from './modules/home/HomePage'
 import { MessagesPage } from './modules/messages/MessagesPage'
 import { MoodPage } from './modules/mood/MoodPage'
 import { CounterPage } from './modules/counter/CounterPage'
+import { MemoriesPage } from './modules/memories/MemoriesPage'
+import { NewMemoryPage } from './modules/memories/NewMemoryPage'
+import { MemoryDetailPage } from './modules/memories/MemoryDetailPage'
+import { MapPage } from './modules/map/MapPage'
 
 export default function App() {
   return (
@@ -18,6 +22,10 @@ export default function App() {
               <Route path="/messages" element={<MessagesPage />} />
               <Route path="/mood" element={<MoodPage />} />
               <Route path="/counter" element={<CounterPage />} />
+              <Route path="/memories" element={<MemoriesPage />} />
+              <Route path="/memories/new" element={<NewMemoryPage />} />
+              <Route path="/memories/:id" element={<MemoryDetailPage />} />
+              <Route path="/map" element={<MapPage />} />
             </Routes>
           </AppLayout>
         </RequireAuth>
